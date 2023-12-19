ba1689c17387aa23c5f1963cfd60491c1361ce35
Polvere243 <danielercoli@gmail.com>
2023-12-19 23:52:11 +0100

    Failed even before starting

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 // recupero le informazioni dall'utente e dichiaro una costante
-
 const distanceToGo = document.getElementById(distance);
 const passengerAge = document.getElementById(age);
+
+const button = document.getElementById(generate);
+
