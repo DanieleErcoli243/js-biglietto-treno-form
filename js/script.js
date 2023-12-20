b40a4d8a16a28678031ef04607ff9974da8e3c57
Polvere243 <danielercoli@gmail.com>
2023-12-20 17:41:50 +0100

    got button from html

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@ const distanceToGo = document.getElementById(distance);
 const passengerAge = document.getElementById(age);
 
 const button = document.getElementById(generate);
-
+console.log(button);
+button.addEventListener("click", function)
