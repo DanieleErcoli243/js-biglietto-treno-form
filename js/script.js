c3f3f9cff511916fb50fb54278777767733ec492
Polvere243 <danielercoli@gmail.com>
2023-12-24 18:41:16 +0100

    finally managed to make a click work

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ const distanceToGo = document.getElementById("distance");
 const passengerAge = document.getElementById("age");
 const confirmButton = document.getElementById("generate");
 const resetButton = document.getElementById("reset");
-const discountGift = document.quesrySelector(".gift");
-const trainCarriage = document.quesrySelector(".carriage");
-const code = document.quesrySelector(".cp-code");
-const finalPrice = document.quesrySelector(".final-price");
+const discountGift = document.querySelector(".gift");
+const trainCarriage = document.querySelector(".carriage");
+const code = document.querySelector(".cp-code");
+const finalPrice = document.querySelector(".final-price");
 const ticketSection = document.querySelector(".ticket-row.d-none");
 
 // creo le variabili di base
@@ -17,3 +17,9 @@ const pricePerKm = 0.21;
 const underDiscount = 20;
 const overDiscount = 40;
 let giftType = "Offerta Standard";
+
+// faccio reagire il botton di conferma al click
+
+confirmButton.addEventListener("click", function(){
+    console.log("cliccato");
+})
